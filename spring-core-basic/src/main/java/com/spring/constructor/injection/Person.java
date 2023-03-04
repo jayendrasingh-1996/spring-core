894db46747a2e5f7b9fb84a459e63baba4493e86
jayendrasingh-1996 <jayendrathakur1996@gmail.com>
2023-03-05 01:11:11 +0530

    adding list into constructor injection

--- spring-core-basic/src/main/java/com/spring/constructor/injection/Person.java
+++ spring-core-basic/src/main/java/com/spring/constructor/injection/Person.java
@@ -1,23 +1,29 @@
 package com.spring.constructor.injection;
 
+import java.util.List;
+
 public class Person {
 
 	private String name;
 	private int personId;
 
+	private List<String> mobileNo;
+
 	private Address address;
 
-	public Person(String name, int personId, Address address) {
+	public Person(String name, int personId, Address address, List<String> mobileNo) {
 		super();
 		this.name = name;
 		this.personId = personId;
+		this.mobileNo = mobileNo;
 		this.address = address;
+	
 	}
 
 	@Override
 	public String toString() {
-		return "Person [name=" + name + ", personId=" + personId + ", address=" + address + "]";
+		return "Person [name=" + name + ", personId=" + personId + ", mobileNo=" + mobileNo + ", address=" + address
+				+ "]";
 	}
 
-	
 }
